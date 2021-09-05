75777f006a49c5e356db7a060889f882b25b300f
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2021-09-05 11:53:36 +0200

    changed a structure of the project and added first component header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import styles from './header.module.scss';
+
+const Header: FunctionComponent = () => {
+  return <header className={styles.header}>header</header>;
+};
+
+export default Header;
