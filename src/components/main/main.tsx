147324229a50905c67b9058dd550004b8798401c
Grzegorz Domaradzki <domaradzki.g@gmail.com>
2021-09-06 17:47:41 +0200

    added library fuze.js to help searching data based on a query, adjusted structure after search and displayed it

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,10 +1,15 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
+import Fuse from 'fuse.js';
+
 import Faqview from '../faqview/faqview';
+import Faqlist from '../faqlist/faqlist';
+import Faqbox from '../faqbox/faqbox';
 
 import { getDataPromise } from '../../services/getData';
 import { Question, Group, Groups, Item } from '../../types/types';
 
 import styles from './main.module.scss';
+import Faqcolumn from '../faqcolumn/faqcolumn';
 
 const Main: FunctionComponent = () => {
   const [data, setData] = useState<Item[]>([]);
@@ -27,12 +32,41 @@ const Main: FunctionComponent = () => {
       setGroups(groupsData);
     });
   }, []);
-  console.log(data);
-  console.log(groups);
+  const options = {
+    useExtendedSearch: false,
+    threshold: 0.6,
+    keys: ['title', 'content'],
+  };
+  const query = 'rig';
+  const fuse = new Fuse(data, options);
+  const result = fuse.search(query ? `'${query}` : ' ');
+  const flatResult = result.map((item) => ({ ...item.item }));
+  const renderData = Object.values(groups).map((side: Group[]) => {
+    return side.map((category: Group) => ({
+      ...category,
+      questions: flatResult.filter(
+        (question) => question.groupId === category.id
+      ),
+    }));
+  });
   return (
     <main className={styles.main}>
       <Faqview>
-        <div>main</div>
+        {renderData.map((column) => (
+          <Faqcolumn key={renderData.indexOf(column)}>
+            {column.map((item) => {
+              return (
+                item.questions.length > 0 && (
+                  <Faqlist key={item.name} categoryName={item.name}>
+                    {item.questions.map((question: Question) => (
+                      <Faqbox key={question.id} title={question.title}></Faqbox>
+                    ))}
+                  </Faqlist>
+                )
+              );
+            })}
+          </Faqcolumn>
+        ))}
       </Faqview>
     </main>
   );
